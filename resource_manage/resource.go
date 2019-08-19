e670ae9eab8e9625a4b80662c4aad2357739fdb6
NakagamiYuta <guguru0014@gmail.com>
2019-08-19 17:39:51 +0900

    [add] Resource::Updateを実装した

--- resource_manage/resource.go
+++ resource_manage/resource.go
@@ -2,6 +2,7 @@ package resourcemanage
 
 import (
 	"fmt"
+	"os/exec"
 	"strconv"
 	"strings"
 )
@@ -10,6 +11,29 @@ import (
 func ResourceInit() Resource {
 	res := Resource{}
 	return res
+}
+
+// Update : リソース情報を最新のものに更新する
+func (r Resource) Update() {
+	// vmstat実行
+	vmstatResult, err := exec.Command("vmstat").Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// 実行結果パース
+	vmstatInfo := vmstatParse(vmstatResult)
+	r.CPU.User = vmstatInfo["CPUUser"]
+	r.CPU.System = vmstatInfo["CPUSystem"]
+	r.CPU.Idol = vmstatInfo["CPUIdol"]
+	r.CPU.Wait = vmstatInfo["CPUWait"]
+	r.IO.In = vmstatInfo["IOIn"]
+	r.IO.Out = vmstatInfo["IOOut"]
+	r.Memory.Buff = vmstatInfo["MemBuff"]
+	r.Memory.Cache = vmstatInfo["MemCache"]
+	r.Memory.Free = vmstatInfo["MemFree"]
+	r.Swap.In = vmstatInfo["SwapIn"]
+	r.Swap.Out = vmstatInfo["SwapOut"]
 }
 
 // vmstatParse : vmstat実行結果から値を読みとる
