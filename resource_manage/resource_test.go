f0f528e30abef13a11032860081b40bd5606007d
NakagamiYuta <guguru0014@gmail.com>
2019-08-19 17:37:02 +0900

    [add] vmstarParse関数のテストを書いた

--- resource_manage/resource_test.go
+++ resource_manage/resource_test.go
@@ -7,6 +7,25 @@ import (
 func TestResourceInit(t *testing.T) {
 	r := ResourceInit()
 	if r.CPU.User != 0 {
-		t.Fatal("ResourceInit() test failed!")
+		t.Fail()
+	}
+}
+
+func TestVmstatParse(t *testing.T) {
+	vmstatResultStr :=
+		"procs -----------memory---------- ---swap-- -----io---- -system-- ------cpu-----\n" +
+			" r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa st\n" +
+			" 2  0 585860   6484  19780 169544    0    0     4     4   11    4  1  0 99  0  0"
+	vmstatResult := []byte(vmstatResultStr)
+	vmstatInfo := vmstatParse(vmstatResult)
+
+	if vmstatInfo["MemFree"] != 6484 {
+		t.Fail()
+	}
+	if vmstatInfo["CPUUser"] != 1 {
+		t.Fail()
+	}
+	if vmstatInfo["IOIn"] != 4 {
+		t.Fail()
 	}
 }
