0e8b5dcda089cb960bfdf05c3658630b84b6fea6
NakagamiYuta <guguru0014@gmail.com>
2019-08-19 16:15:47 +0900

    [add] リソース情報を扱う構造体を定義した

---
+++ resource_manage/struct.go
@@ -0,0 +1,36 @@
+package resource_manage
+
+// Resource : サーバリソースを扱う
+type Resource struct {
+	CPU    CPUResource
+	Memory MemoryResource
+	IO     IOResource
+	Swap   SwapResource
+}
+
+// CPUResource : CPU
+type CPUResource struct {
+	User   int
+	System int
+	Idol   int
+	Wait   int
+}
+
+// MemoryResource : メモリ
+type MemoryResource struct {
+	Free  int
+	Buff  int
+	Cache int
+}
+
+// IOResource : IO
+type IOResource struct {
+	In  int
+	Out int
+}
+
+// SwapResource : スワップ
+type SwapResource struct {
+	In  int
+	Out int
+}
