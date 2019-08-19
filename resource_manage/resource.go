569d431dadbcad6dc550cf4314a2bb12012ebfd6
NakagamiYuta <guguru0014@gmail.com>
2019-08-19 17:42:54 +0900

    [clean] vmstatParseを別ファイルに持っていった

--- resource_manage/resource.go
+++ resource_manage/resource.go
@@ -3,8 +3,6 @@ package resourcemanage
 import (
 	"fmt"
 	"os/exec"
-	"strconv"
-	"strings"
 )
 
 // ResourceInit : Resourceを初期化して返す
@@ -34,33 +32,4 @@ func (r Resource) Update() {
 	r.Memory.Free = vmstatInfo["MemFree"]
 	r.Swap.In = vmstatInfo["SwapIn"]
 	r.Swap.Out = vmstatInfo["SwapOut"]
-}
-
-// vmstatParse : vmstat実行結果から値を読みとる
-func vmstatParse(vmstatResult []byte) map[string]int {
-	// 読み取り準備
-	resultStr := string(vmstatResult)
-	infoArray := strings.Split(strings.Split(resultStr, "\n")[2], " ")
-	keys := []string{
-		"ProcessR", "ProcessB", "MemSwap", "MemFree", "MemBuff", "MemCache", "SwapIn", "SwapOut",
-		"IOIn", "IOOut", "SystemIn", "SystemCs", "CPUUser", "CPUSystem", "CPUIdol", "CPUWait", "CPUSt",
-	}
-
-	// 値読み取り
-	var err error
-	keyIdx := 0
-	vmstatInfo := make(map[string]int)
-	fmt.Println(infoArray)
-	for _, elem := range infoArray {
-		if elem == "" {
-			continue
-		}
-
-		vmstatInfo[keys[keyIdx]], err = strconv.Atoi(elem)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		keyIdx++
-	}
-	return vmstatInfo
 }
---
+++ resource_manage/vmstat.go
@@ -0,0 +1,36 @@
+package resourcemanage
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// vmstatParse : vmstat実行結果から値を読みとる
+func vmstatParse(vmstatResult []byte) map[string]int {
+	// 読み取り準備
+	resultStr := string(vmstatResult)
+	infoArray := strings.Split(strings.Split(resultStr, "\n")[2], " ")
+	keys := []string{
+		"ProcessR", "ProcessB", "MemSwap", "MemFree", "MemBuff", "MemCache", "SwapIn", "SwapOut",
+		"IOIn", "IOOut", "SystemIn", "SystemCs", "CPUUser", "CPUSystem", "CPUIdol", "CPUWait", "CPUSt",
+	}
+
+	// 値読み取り
+	var err error
+	keyIdx := 0
+	vmstatInfo := make(map[string]int)
+	fmt.Println(infoArray)
+	for _, elem := range infoArray {
+		if elem == "" {
+			continue
+		}
+
+		vmstatInfo[keys[keyIdx]], err = strconv.Atoi(elem)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		keyIdx++
+	}
+	return vmstatInfo
+}
