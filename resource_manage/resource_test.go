a87b38c9cb07d2fee2d0de3c7c67b55f635a5cc1
NakagamiYuta <guguru0014@gmail.com>
2019-08-20 14:05:32 +0900

    [change] Resource構造体がもつレシーバ関数の引数を変えた
    実体ではなく、ポインタで扱うようにした

--- resource_manage/resource_test.go
+++ resource_manage/resource_test.go
@@ -5,13 +5,13 @@ import (
 )
 
 func TestResourceInit(t *testing.T) {
-	r := ResourceInit()
+	r := AllocResourceStruct()
 	if r.CPU.User != 0 {
 		t.Fail()
 	}
 }
 
 func TestResourceUpdate(t *testing.T) {
-	r := ResourceInit()
+	r := AllocResourceStruct()
 	r.Update()
 }
