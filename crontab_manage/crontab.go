10507b944a5b2da61af363cfbeee59d61970611a
NakagamiYuta <guguru0014@gmail.com>
2019-08-21 16:04:15 +0900

    [add] Crontabの最新設定情報を取得する関数を作成

--- crontab_manage/crontab.go
+++ crontab_manage/crontab.go
@@ -1,7 +1,22 @@
 package crontabmanage
 
+import (
+	"fmt"
+	"os/exec"
+)
+
 // AllocCrontabStruct : Crontab構造体を新しく作って返す
 func AllocCrontabStruct() *Crontab {
 	c := Crontab{}
 	return &c
+}
+
+// GetLatestCrontabData : Crontab情報を更新する
+func GetLatestCrontabData() []*Crontab {
+	result, err := exec.Command("crontab", "-l").Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return crontabResultParse(result)
 }
