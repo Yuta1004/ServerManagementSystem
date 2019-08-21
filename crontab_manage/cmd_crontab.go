2f5f4e3911b0b81807bff2b1c4667fade4f4fafa
NakagamiYuta <guguru0014@gmail.com>
2019-08-21 15:44:55 +0900

    [add] Crontab実行結果をパースする関数を作った

---
+++ crontab_manage/cmd_crontab.go
@@ -0,0 +1,61 @@
+package crontabmanage
+
+import (
+	"server-manage/common"
+	"strconv"
+	"strings"
+)
+
+func crontabResultParse(result []byte) []*Crontab {
+	resultStr := string(result)
+	resultArray := strings.Split(resultStr, "\n")
+	crontabInfoArray := make([]*Crontab, len(resultArray))
+
+	for idx, line := range resultArray {
+		crontabInfo := strings.Split(line, " ")
+		crontab := AllocCrontabStruct()
+		crontab.Minute = crontabDateParse(crontabInfo[0], 0, 59)
+		crontab.Hour = crontabDateParse(crontabInfo[1], 0, 23)
+		crontab.DayOfMonth = crontabDateParse(crontabInfo[2], 1, 31)
+		crontab.Month = crontabDateParse(crontabInfo[3], 1, 12)
+		crontab.Day = crontabDateParse(crontabInfo[4], 0, 6)
+		crontab.Command = crontabInfo[5]
+		crontabInfoArray[idx] = crontab
+	}
+
+	return crontabInfoArray
+}
+
+func crontabDateParse(date string, lower, upper int) CrontabDateNum {
+	dateArray := []int{}
+	wildcardFlag := false
+
+	for _, elem := range strings.Split(date, ",") {
+		// ワイルドカード
+		if strings.Contains(elem, "*") {
+			wildcardFlag = true
+			interval := 1
+			if strings.Contains(elem, "/") {
+				interval, _ = strconv.Atoi(strings.Split(elem, "/")[1])
+			}
+			dateArray = append(
+				dateArray,
+				makeArrayWithInterval(lower, upper, interval)...,
+			)
+		}
+
+		// その他
+		convertedI, _ := strconv.Atoi(elem)
+		dateArray = append(dateArray, convertedI)
+	}
+
+	return CrontabDateNum{wildcardFlag, common.DeduplicationArrayInt(dateArray)}
+}
+
+func makeArrayWithInterval(lower, upper, interval int) []int {
+	retArray := make([]int, 0)
+	for num := lower; num <= upper; num += interval {
+		retArray = append(retArray, num)
+	}
+	return retArray
+}
---
+++ crontab_manage/cmd_crontab_test.go
@@ -0,0 +1,31 @@
+package crontabmanage
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCrontabResultParse(t *testing.T) {
+	crontabResult :=
+		"0 0,12 */1 * * command\n" +
+			"0 0 * * 0 command\n" +
+			"0 * * * * command\n" +
+			"30 6,11,17 * * * command"
+	result := crontabResultParse([]byte(crontabResult))
+
+	if len(result) != 4 {
+		t.Fail()
+	}
+	if result[0].Minute.wildcard {
+		t.Fail()
+	}
+	if !reflect.DeepEqual(result[0].Minute.nums, []int{0}) {
+		t.Fail()
+	}
+	if !reflect.DeepEqual(result[0].Hour.nums, []int{0, 12}) {
+		t.Fail()
+	}
+	if !reflect.DeepEqual(result[0].Day.nums, []int{0, 1, 2, 3, 4, 5, 6}) {
+		t.Fatal(result[0])
+	}
+}
