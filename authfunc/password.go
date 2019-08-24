d75b8f14faa11e4088fa25d467b744f85fccb271
NakagamiYuta <guguru0014@gmail.com>
2019-08-24 15:10:45 +0900

    [add] パスワード認証関連の関数を作成した

---
+++ authfunc/password.go
@@ -0,0 +1,21 @@
+package authfunc
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
+
+// GenPasswordHash : パスワードのハッシュを生成して返す
+func GenPasswordHash(rawPassword string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(rawPassword), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return string(hash)
+}
+
+// AuthPassword : パスワードとハッシュ化されたパスワードが合致するかを返す
+func AuthPassword(rawPassword, hashPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(rawPassword), []byte(hashPassword))
+	return err != nil
+}
