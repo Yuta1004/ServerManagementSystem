1fdf0cd5fd4b93048cc76cf0c95d2cbebb43b7a8
NakagamiYuta <guguru0014@gmail.com>
2019-08-25 15:35:37 +0900

    [add] ユーザ情報をDBに保存する関数を作成した

--- db/user_info.go
+++ db/user_info.go
@@ -7,6 +7,44 @@ import (
 	_ "github.com/go-sql-driver/mysql"	/* => database/sqlで参照する */
 )
 
+// InsertUserDataToDB : ユーザ情報をDBに追加する
+func InsertUserDataToDB(userID, hashPassword string) (insertResult bool) {
+	insertResult = false
+
+	// DB接続
+	connect, err := getDBConnect()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	defer connect.Close()
+
+	// トランザクション関連処理
+	tx, err := connect.Begin()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	defer func() {
+		result := recover()
+		if result != nil {
+			tx.Rollback()
+			log.Println("[INFO] Executed the rollback of DB in \"InsertUserDataToDB\" function.")
+			insertResult = false
+		} else {
+			tx.Commit()
+		}
+	}()
+
+	// SQL実行
+	_, err = tx.Exec("insert into user values(?, ?)", userID, hashPassword)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	return
+}
+
 // GetUserDataFromDB : DBからユーザ情報を引っ張ってくる
 func GetUserDataFromDB() *[]UserInfo {
 	userInfoList := make([]UserInfo, 0)
--- db/user_info_test.go
+++ db/user_info_test.go
@@ -2,11 +2,18 @@ package db
 
 import (
 	"testing"
+	"server-manage/authfunc"
 )
 
-func TestGetUserInfo(t *testing.T) {
+func TestControllUserDB(t *testing.T) {
+	hashPassword := authfunc.GenPasswordHash("servermanage")
+	result := InsertUserDataToDB("testuser", hashPassword)
+	if result {
+		t.Fail()	// すでにtestuserが挿入された状態でテストを行うこと!
+	}
+
 	userInfoList := *GetUserDataFromDB()
-	if len(userInfoList) != 0 {
+	if len(userInfoList) != 1 {
 		t.Fail()
 	}
 }
