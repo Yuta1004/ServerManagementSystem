f871b8b92657d1445ae4c16bd81e200330705510
NakagamiYuta <guguru0014@gmail.com>
2019-08-27 16:28:22 +0900

    [add] ログイン認証処理を書いた

--- controller/login.go
+++ controller/login.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"net/url"
 	"github.com/gin-gonic/gin"
+	"server-manage/authfunc"
 )
 
 // LoginPageController : ログイン画面のコントローラー
@@ -15,19 +16,26 @@ func LoginPageController(c *gin.Context) {
 
 // LoginPagePostController : ログイン画面(POST)のコントローラー
 func LoginPagePostController(c *gin.Context) {
+	// 入力確認
 	c.Request.ParseForm()
 	userID := c.Request.Form["userid"][0]
 	password := c.Request.Form["password"][0]
 	if userID == "" || password == "" {
-		redirectWithError(c, "login", "入力に不備があります")
-		return
+		redirectWithError(c, "login", "InvalidInput")
 	}
 
-	c.Redirect(302, "")
+	// ログイン処理
+	if authfunc.Login(userID, password) {
+		c.Redirect(302, "")
+	} else {
+		redirectWithError(c, "login", "WrongInput")
+	}
+	c.Next()
 }
 
 
 func redirectWithError(c *gin.Context, location, message string) {
 	encodedMessage := url.QueryEscape(message)
 	c.Redirect(302, location+"?error="+encodedMessage)
+	c.Abort()
 }
