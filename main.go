a48a0b62f8324aec21573972fe06c08d7267fb4a
NakagamiYuta <guguru0014@gmail.com>
2019-08-28 16:46:42 +0900

    [add] /manage配下のページについてセッションチェックをするように

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"os"
 	"server-manage/controller"
-
-	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-contrib/multitemplate"
+
 )
 
 func main() {
@@ -14,10 +17,18 @@ func main() {
 	router.Static(baseURL+"/static", "./static")
 	router.HTMLRender = createHTMLRender()
 
+	store := cookie.NewStore([]byte(os.Getenv("COOKIE_SECRET")))
+	router.Use(sessions.Sessions("GoServerManageSession", store))
+
 	router.GET(baseURL+"/", controller.TopPageController)
-	router.GET(baseURL+"/crontab", controller.CrontabPageController)
 	router.GET(baseURL+"/login", controller.LoginPageController)
 	router.POST(baseURL+"/login", controller.LoginPagePostController)
+
+	menu := router.Group(baseURL+"/manage")
+	menu.Use(sessionCheck())
+	{
+		menu.GET("/crontab", controller.CrontabPageController)
+	}
 
 	router.Run(":19000")
 }
@@ -28,4 +39,19 @@ func createHTMLRender() multitemplate.Renderer {
 	render.AddFromFiles("crontab", "templates/base.html", "templates/crontab.html", "templates/navbar.html")
 	render.AddFromFiles("login", "templates/base.html", "templates/login.html", "templates/navbar.html")
 	return render
+}
+
+func sessionCheck() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		userID := session.Get("UserID")
+
+		if userID == nil {
+			c.Redirect(302, "../login")
+			c.Abort()
+		} else {
+			c.Set("UserID", userID)
+			c.Next()
+		}
+	}
 }
