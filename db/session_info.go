1e1873a4d78aa574530f216aec2f19dface0c67e
NakagamiYuta <guguru0014@gmail.com>
2019-08-28 15:58:09 +0900

    [add] InsertSessionDataToDBを作成した

--- db/session_info.go
+++ db/session_info.go
@@ -6,6 +6,23 @@ import (
 	"server-manage/common"
 )
 
+// InsertSessionDataToDB : セッションデータをDBに記録、既にidが存在していたら更新処理
+func InsertSessionDataToDB(userID, passphrase string, expirationUnixTime int) bool {
+	executable :=
+		func (tx *sql.Tx) {
+			// SQL実行
+			sql := "insert into session values(?, ?, ?)" +
+					" on duplicate key update passphrase=?, expiration_unix_time=?"
+			_, err := tx.Exec(sql, userID, passphrase, expirationUnixTime, passphrase, expirationUnixTime)
+			if err != nil {
+				log.Println(err.Error())
+				panic("An error happened in \"InsertSessionData\" function.")
+			}
+		}
+
+	result := ControlDBWithTransaction("InsertSessionData", executable)
+	return result
+}
 
 // GetSessionDataFromDB : セッションデータを取り出す
 func GetSessionDataFromDB(request ...string) *[]SessionInfo {
@@ -13,7 +30,7 @@ func GetSessionDataFromDB(request ...string) *[]SessionInfo {
 	executable :=
 		func (conn *sql.DB) interface{} {
 			// SQL実行
-			sql := "select * from user where id " + common.MakeSQLINOperator(request)
+			sql := "select * from session where id " + common.MakeSQLINOperator(request)
 			result, err := conn.Query(sql, common.ConvToInterfaceSlice(request)...)
 			if err != nil {
 				log.Println(err.Error())
@@ -24,7 +41,11 @@ func GetSessionDataFromDB(request ...string) *[]SessionInfo {
 			// データ取り出し
 			for result.Next() {
 				var sessionInfo SessionInfo
-				err := result.Scan(&sessionInfo.ID, &sessionInfo.Passphrase, &sessionInfo.ExpirationUnixTime)
+				err := result.Scan(
+					&sessionInfo.ID,
+					&sessionInfo.Passphrase,
+					&sessionInfo.ExpirationUnixTime,
+				)
 				if err != nil {
 					log.Println(err.Error())
 					return &sessionInfoList
