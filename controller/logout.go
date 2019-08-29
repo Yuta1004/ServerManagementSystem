875b2f5f8d4e6478fe556a45db4e3b3415d69d38
NakagamiYuta <guguru0014@gmail.com>
2019-08-29 23:55:31 +0900

    [add] controller/logout.goでCheckOKSessionを使うようにした

--- controller/logout.go
+++ controller/logout.go
@@ -2,16 +2,14 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/gin-contrib/sessions"
 	"server-manage/authfunc"
 )
 
 func LogoutPageController(c *gin.Context) {
-	session := sessions.Default(c)
-	loginNow := session.Get("UserID") != nil
+	checkLogin, _ := authfunc.CheckOKSession(c)
 	authfunc.Logout(c)
 	c.HTML(200, "logout", gin.H{
 		"pageTitle": "SMS-Logout",
-		"loginNow": loginNow,
+		"loginNow": checkLogin,
 	})
 }
