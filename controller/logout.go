577324cdd0443b0a5921ea720916ac28809af900
NakagamiYuta <guguru0014@gmail.com>
2019-08-29 21:30:04 +0900

    [change] ログアウトページの仕様を変更

--- controller/logout.go
+++ controller/logout.go
@@ -2,10 +2,15 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/sessions"
 	"server-manage/authfunc"
 )
 
 func LogoutPageController(c *gin.Context) {
+	session := sessions.Default(c)
+	loginNow := session.Get("UserID") != nil
 	authfunc.Logout(c)
-	c.HTML(200, "logout", gin.H{})
+	c.HTML(200, "logout", gin.H{
+		"loginNow": loginNow,
+	})
 }
