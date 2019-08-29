e6be805127d3b5d84c98e98c479ffd624c14f649
NakagamiYuta <guguru0014@gmail.com>
2019-08-29 23:53:43 +0900

    [add] main.goでCheckOKSessionを使うようにした

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"os"
-	"time"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-contrib/multitemplate"
 	"server-manage/controller"
-	"server-manage/db"
+	"server-manage/authfunc"
 )
 
 func main() {
@@ -50,32 +49,12 @@ func createHTMLRender() multitemplate.Renderer {
 
 func sessionCheck() gin.HandlerFunc{
 	return func(c *gin.Context) {
-		session := sessions.Default(c)
-		userID := session.Get("UserID")
-		passphrase := session.Get("Passphrase")
-
-		if userID == nil {
-			c.Redirect(302, "../login")
+		checkResult, message := authfunc.CheckOKSession(c)
+		if checkResult {
+			c.Next()
+		} else {
+			c.Redirect(302, "../login?error=" + message)
 			c.Abort()
-            return
 		}
-
-		// 有効期限チェック
-		userIDStr := userID.(string)
-		sessionDBData := (*db.GetSessionDataFromDB(userIDStr))[0]
-		if time.Now().Unix() > int64(sessionDBData.ExpirationUnixTime) {
-			c.Redirect(302, "../login?error=OverExpirationDate")
-			c.Abort()
-            return
-		}
-
-		// パスフレーズチェック
-		passphraseStr := passphrase.(string)
-		if passphraseStr != sessionDBData.Passphrase {
-			c.Redirect(302, "../login?error=FraudSession")
-			c.Abort()
-            return
-		}
-		c.Next()
 	}
 }
