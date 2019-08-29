07399f76830163f66e5ea71159e68d708bc5b7da
NakagamiYuta <guguru0014@gmail.com>
2019-08-30 01:12:31 +0900

    [add] UpdateCommandDataOfDB関数作成

--- db/command_info.go
+++ db/command_info.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"log"
+	"strings"
 	"database/sql"
 )
 
@@ -18,5 +19,33 @@ func InsertCommandDataToDB(userID, name, command string) bool {
 		}
 
 	result := ControlDBWithTransaction("InsertCommandData", executable)
+	return result
+}
+
+func UpdateCommandDataOfDB(id int, userID string, request map[string]interface{}) bool {
+	// 更新するキー, 値を取り出す
+	reqKeySqls := make([]string, 0)
+	reqValues := make([]interface{}, 0)
+	for key, value := range request {
+		reqKeySqls = append(reqKeySqls, key+"=?")
+		reqValues = append(reqValues, value)
+	}
+
+	// SQL組み立て
+	sqlBody := "update command set " + strings.Join(reqKeySqls, ", ") + " where id=? and user_id=?"
+	reqValues = append(reqValues, id)
+	reqValues = append(reqValues, userID)
+
+	executable :=
+		func (tx *sql.Tx) {
+			// SQL実行
+			_, err := tx.Exec(sqlBody, reqValues...)
+			if err != nil {
+				log.Println(err.Error())
+				panic("An error happened in \"UpdateCommandData\" function.")
+			}
+		}
+
+	result := ControlDBWithTransaction("UpdateCommandData", executable)
 	return result
 }
